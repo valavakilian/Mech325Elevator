35140e72cecb36be240e1bb3c54bc21afbe74c03
vala <mahvaala@yahoo.com>
2019-11-23 16:05:53 -0800

    second commit before cailins commit

--- bearing_analysis.py
+++ bearing_analysis.py
@@ -1,45 +1,115 @@
-V_max = 1
-PV_max = 1
-P_max = 1
-T_max = 1
-D = 1
-F = 1
-N = 1
-t = 1
-L = 1
+import numpy as np
+# import matplotlib.plt as plt 
+from scipy.special import gamma, factorial
 
 
-def get_f1_rotary(pressure, velocity):
+
+
+class bearing_system():
+    a = 3.0
     
-    if ( velocity < 0 or velocity >= 100 ) or ( pressure > 3600 or pressure < 0 ):
-        return False
+    def __init__(C_10, inner_diameter, outer_diameter, L_R, F_R, R, x_0, b, theta, num_bearings):
+        self.C_10 = C_10
+        self.inner_diameter = inner_diameter
+        self.outer_diameter = outer_diameter
+        self.L_R = L_R
+        self.F_R = F_R
+        self.R = R
+        self.x_0 = x_0
+        self.b = b
+        self.theta
+        self.num_bearings
+        
+    # L_D is the number of revolutions in three years for this component
+    def return_largest_force(acceleration, F_D, L_D, a_f, R_D):
+        R_A = R_D ** (1.0 / self.num_bearings)
+        L_10 = self.L_R
+        x_D = self.L_D / self.L_R
+        F_limit = (self.C_10 / a_f) * ( ( ( self.x_0 + (self.theta - self.x_0) * (math.log(1 / R_A)) ** ( 1.0/b ) ) / ( x_D ) ) ** ( 1.0/a ) )
+        if F_limit < F_D:
+            return False
+        else:
+            return True
     
-    f1 = 0
-    if pressure < 720:
-        if velocity <= 3.3:
-            f1 = 1.0
-        elif velocity <= 33:
-            slope = (1.3 - 1.0) / (33 - 3.3)
-            f1 = slope * (velocity - 3.3) + 1.0 
-        elif valocity <= 100:
-            slope = (1.8 - 1.3) / (100.0 - 33.0)
-            f1 = slope * (velocity - 33.0) + 1.3
-    elif pressure < 3600:
-        if velocity <= 3.3:
-            f1 = 1.5
-        elif velocity <= 33:
-            slope = (2.0 - 1.5) / (33 - 3.3)
-            f1 = slope * (velocity - 3.3) + 1.5 
-        elif valocity <= 100:
-            slope = (2.7 - 2.0) / (100.0 - 33.0)
-            f1 = slope * (velocity - 33.0) + 2.0
 
-    return f1
+        
 
 
-def get_f2(temperature, foreign_matter):
-    
-    if temperature > 210 :
-        return False
 
-    return f2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+catalog_rating_force = 1
+rating_speed = 1
+rating_life_in_hours = 1
+a = 3 # ball bearings
+# R = np.exp()
+
+x_0 = 1.0
+b = 1.0
+theta = 1.0
+
+
+R_manufacturer = 1.0
+F_R = 1.0
+l_R = 1.0
+n_R = 1.0
+L_R = l_R * n_R * 60
+C_10 = 1.0
+# We need to find 
+
+
+# We use two bearings and they have the same design factors
+# Lifetime of 30 complete cycles per day, 5 days a week for 3 years
+time_it_takes_for_each_cycle = 10.0
+L_D = 30 * 5 * 3 * time_it_takes_for_each_cycle
+
+
+x_D = L_D / L_R
+mu_x = x_0 + (theta - x_0) * gamma(1.0 + 1.0 / b)
+sigma_x = (theta - x_0) * (gamma(1.0 + 2.0 / b) - gamma(1.0 + 1.0 / b) ** 2) ** 0.5
+
+
+# functions 
+R = 0.9
+R_D = 0.99
+
+def valid_setting(F_D, acceleration, Distance, F_R, ):
+
+
+
+
+
+
---
+++ bushing_analysis.py
@@ -0,0 +1,65 @@
+V_max = 1
+PV_max = 1
+P_max = 1
+T_max = 1
+D = 1
+F = 1
+N = 1
+t = 1
+L = 1
+
+
+def get_f1_rotary(pressure, velocity):
+    
+    if ( velocity < 0 or velocity >= 100 ) or ( pressure > 3600 or pressure < 0 ):
+        return False
+    
+    f1 = 0
+
+    if pressure < 720:
+        if velocity <= 3.3:
+            f1 = 1.0
+        elif velocity <= 33:
+            slope = (1.3 - 1.0) / (33 - 3.3)
+            f1 = slope * (velocity - 3.3) + 1.0 
+        elif valocity <= 100:
+            slope = (1.8 - 1.3) / (100.0 - 33.0)
+            f1 = slope * (velocity - 33.0) + 1.3
+    
+    elif pressure < 3600:
+        if velocity <= 3.3:
+            f1 = 1.5
+        elif velocity <= 33:
+            slope = (2.0 - 1.5) / (33 - 3.3)
+            f1 = slope * (velocity - 3.3) + 1.5 
+        elif valocity <= 100:
+            slope = (2.7 - 2.0) / (100.0 - 33.0)
+            f1 = slope * (velocity - 33.0) + 2.0
+
+    return f1
+
+
+def get_f2(temperature, foreign_matter):
+    
+    if temperature > 210 :
+        return False
+    
+    f2 = 0
+
+    if temperature <= 140:
+        if foreign_matter is False:
+            # unsure 
+            slope = (6.0 - 3.0) / (140.0 - 0)
+            f2 = slope * (temperature - 0) + 3.0
+        else:
+            f2 = 1.0
+
+    elif temperature <= 210:
+        if foreign_matter is False:
+            slope = (6.0 - 3.0) / (210.0 - 140.0)
+            f2 = slope * (temperature - 140.0) + 3.0
+        else:
+            slope = (12.0 - 6.0) / (210.0 - 140.0)
+            f2 = slope * (temperature - 140.0) + 6.0
+
+    return f2
